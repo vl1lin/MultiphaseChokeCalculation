55e9fab39e0cd44177e07c79c5ce509efd092495
vl1lin <v1adbarten@mail.ru>
2026-04-03 00:49:59 +0300

    Исправил настройку логгера

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -1,7 +1,17 @@
 import logging
 import sys
-from datetime import datetime
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+
+def get_log_dir() -> Path:
+    if getattr(sys, 'frozen', False):
+        base_path = Path(sys.executable).parent
+    else:
+        base_path = Path(__file__).parent
+    log_dir = base_path / "logs"
+    log_dir.mkdir(exist_ok=True)
+    return log_dir / "app.log"
 
 
 def setup_logger():
@@ -11,14 +21,16 @@ def setup_logger():
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
 
-    file_handler = RotatingFileHandler('app.log', maxBytes=5*1024*1024, backupCount=3, encoding='utf-8')
+    if root_logger.handlers:
+        return
+
+    file_handler = RotatingFileHandler(get_log_dir(), maxBytes=5*1024*1024, backupCount=3, encoding='utf-8')
     file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
 
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.INFO)
 
-    if not root_logger.handlers:
-        root_logger.addHandler(file_handler)
-        root_logger.addHandler(stream_handler)
-
-    return root_logger
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(stream_handler)
