f266fe8badb0f86bf80621cb173c0542d2354a58
vl1lin <v1adbarten@mail.ru>
2026-04-03 01:00:11 +0300

    Проверил настройку логгера

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from utils.logger_config import setup_logger
-
-
 setup_logger()
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.info("Проверка info")
+logger.error("проверка error")
+logger.debug("проверка debug")
+logger.info("коней проверка")
